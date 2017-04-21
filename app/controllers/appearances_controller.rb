2d69297ff3b1079d0bb62201bee3a0074be138ff
markdegroat <mhdegroat@gmail.com>
2017-04-21 10:08:38 -0400

    Have all features working at the basic level, still need to make it automatically associate the user that is currently logged in rather than having the user have to select themselves.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -5,18 +5,35 @@ class AppearancesController < ApplicationController
   end
 
   def create
+
     @appearance = Appearance.new(appearance_params)
+    binding.pry
     if @appearance.save
-      redirect_to @appearance.episode
+      redirect_to apperances_path
     else
       redirect_to new_appearance_path
     end
   end
 
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    #binding.pry
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(appearance_params)
+    redirect_to appearances_path
+  end
+
+  def index
+    @appearances = Appearance.all
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :user_id, :rating)
   end
 
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,10 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
-  
+  #for feature 3
+  belongs_to :user
+
+  #feature 2
+  validates_inclusion_of :rating, in: 0..5
+
 end
